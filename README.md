fd946d6d36a74e234fc559bed327cce1ee75433c
RRITIK11 <sharmaritik5550@gmail.com>
2024-02-14 19:13:07 +0530

    password generator

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # React 
+[React website](https://react.dev/)
+```
+npm : node package manager -> use to install or uninstall a library, package 
+npx : node package executer -> it directly execute 
+```
+**Create a react app**
+> npx create-react-app React_01
+
+It is quite slow
+
+**To start a project :**
+> npm run start
+
+## Vite
+It is a bundler
+
+**Create a vite app**
+```
+npm create vite@latest
+npm install
+```
+**To start a project**
+>npm run dev
+
+
---
+++ React_01/src/App.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(10);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [symbolAllowed, setSymbolAllowed] = useState(false);
+  const [password , setPassword] = useState("");
+
+  function randomPassword(){
+    let str = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    if(numberAllowed) str += "0123456789";
+    if(symbolAllowed) str += "~`!@#$%^&*()_+-={[]}'/<,.>?";
+    let newPassword = "";
+    for(let i=0; i<length ; i++){
+      newPassword += str[Math.round(Math.random()*str.length)];
+    }
+    return newPassword
+  }
+
+  useEffect(()=>{
+    console.log(length, numberAllowed , symbolAllowed, password);
+    setPassword(randomPassword())
+  },[length , numberAllowed , symbolAllowed])
+
+  return (
+    <div className="flex bg-[#121212] h-screen justify-center items-center">
+      <div className="bg-gray-700 w-1/2 max-w-[600px] flex flex-col mx-auto my-10 rounded-2xl items-center px-10 py-5 gap-4">
+        <h1 className="text-5xl text-white text-wrap text-center">Password Generator</h1>
+        <div className="w-full flex rounded-2xl h-12 overflow-hidden flex-nowrap">
+          <input
+            className="px-4 shrink outline-none text-orange-500 font-semibold w-full"
+            type="text"
+            placeholder="password"
+            value={password}
+            readOnly
+          />
+          <button className="grow-0 shrink-0 px-4 bg-blue-700 font-semibold text-white w-18" 
+            onClick={()=>navigator.clipboard.writeText(password)}>
+            Copy
+          </button>
+        </div>
+        <div className="flex gap-6 flex-wrap items-center justify-center">
+          <div className="flex gap-2 flex-wrap justify-center items-center">
+            <input
+              id="lengthInput"
+              type="range"
+              min="6"
+              max="30"
+              value={length}
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label className="text-orange-500 font-bold" htmlFor="lengthInput">
+              Length ({length})
+            </label>
+          </div>
+          <div className="flex gap-2">
+            <input
+              id="numberInput"
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              onChange={()=>{setNumberAllowed((prev)=>!prev)}}
+            />
+            <label className="text-orange-500 font-bold" htmlFor="numberInput">
+              Numbers
+            </label>
+          </div>
+          <div className="flex gap-2">
+            <input
+              id="symbolInput"
+              type="checkbox"
+              defaultChecked={symbolAllowed}
+              onChange={()=>{setSymbolAllowed((prev)=>!prev)}}
+            />
+            <label className="text-orange-500 font-bold" htmlFor="symbolInput">
+              Symbol
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
