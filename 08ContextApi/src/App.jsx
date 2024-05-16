3e9ef7496156098ec21390046b3f2182e394e6c5
RRITIK11 <sharmaritik5550@gmail.com>
2024-05-16 20:21:39 +0530

    Context Api 01

---
+++ 08ContextApi/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Profile from './components/Profile'
+import Login from './components/Login'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>Hello World</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
